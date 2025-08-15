24dc7872a13b6ab9bc4228bf21bceeef29323376
Amruth Kumar M <amruth.kumar.portfolio@gmail.com>
2025-08-15 08:40:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ import streamlit as st
 import joblib
 import numpy as np
 
-# Load the scaler and model from the pkl files
 scaler = joblib.load("scaler.pkl")
 model = joblib.load("model.pkl")
 
@@ -21,8 +20,8 @@ st.write("Please enter the values and hit the predict button for getting a predi
 st.divider()
 
 age = st.number_input("Enter age", min_value=10, max_value=100, value=30)
-tenure = st.number_input("Enter Tenure", min_value=0, max_value=100, value=10) # Corrected max_value to 100
-monthly_charges = st.number_input("Enter Monthly Charges", min_value=0, max_value=130, value=10) # Corrected variable name and min/max values
+tenure = st.number_input("Enter Tenure", min_value=0, max_value=100, value=10) 
+monthly_charges = st.number_input("Enter Monthly Charges", min_value=0, max_value=130, value=10) 
 gender = st.selectbox("Enter the Gender", ["Male", "Female"])
 
 st.divider()
@@ -35,7 +34,6 @@ if predict_button:
     
     X1 = np.array(X)
     
-    # Corrected the error by reshaping the 1D array into a 2D array
     X_array = scaler.transform(X1.reshape(1, -1))
     
     prediction = model.predict(X_array)[0]
